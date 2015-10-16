876acb4e30c344d4b0585aaf5f185dd6efaacb2c
Magnus Hoff <maghoff@gmail.com>
2015-10-16 16:28:21 +0200

    Use doT template for feed show function

--- couchapp/design_doc/shows/feed.js
+++ couchapp/design_doc/shows/feed.js
@@ -1,25 +1,22 @@
 function (doc, req) {
 	var React = require('react/addons');
 
+	var dot = require('dot');
+	var template = dot.template(this.templates["feed"]);
+
 	var FeedConfig = React.createClass({
 		render: function () {
-			return React.createElement("body", null,
-				React.createElement("div", { className: "header" },
-					React.createElement("span", { className: "feedTitle" }, this.props.feed.title),
-					React.createElement("span", { className: "articleTitle" }, "Configuration")
-				),
-				React.createElement("div", { className: "config" },
-					React.createElement("div", { className: "configItem" },
-						React.createElement("p", { className: "configDescription" },
-							"When displaying articles from this feed..."),
-						React.createElement("div", { className: "buttonList" },
-							React.createElement("div",
-								{ className: "button" + (this.props.feed.open_linked ? "" : " selected") },
-								"Show description from feed"),
-							React.createElement("div",
-								{ className: "button" + (this.props.feed.open_linked ? " selected" : "") },
-								"Embed linked article")
-						)
+			return React.createElement("div", { className: "config" },
+				React.createElement("div", { className: "configItem" },
+					React.createElement("p", { className: "configDescription" },
+						"When displaying articles from this feed..."),
+					React.createElement("div", { className: "buttonList" },
+						React.createElement("div",
+							{ className: "button" + (this.props.feed.open_linked ? "" : " selected") },
+							"Show description from feed"),
+						React.createElement("div",
+							{ className: "button" + (this.props.feed.open_linked ? " selected" : "") },
+							"Embed linked article")
 					)
 				)
 			);
@@ -29,8 +26,13 @@ function (doc, req) {
 	start({
 		'content-type': 'text/html; charset=utf-8'
 	});
-	send(this.templates["feed.mu"]);
-	send(React.renderToStaticMarkup(
-		React.createElement(FeedConfig, { feed: doc })
-	));
+	send(
+		template({
+			ddoc: this,
+			feed: doc,
+			body: React.renderToStaticMarkup(
+				React.createElement(FeedConfig, { feed: doc })
+			)
+		})
+	);
 }
