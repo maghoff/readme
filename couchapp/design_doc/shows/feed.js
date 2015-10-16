58587ba886168faa4af8155440f52e55c4d26e01
Magnus Hoff <maghoff@gmail.com>
2015-10-16 17:31:31 +0200

    Display details about feeds in feed show function

--- couchapp/design_doc/shows/feed.js
+++ couchapp/design_doc/shows/feed.js
@@ -2,22 +2,21 @@ function (doc, req) {
 	var React = require('lib/react/addons');
 
 	var dot = require('lib/dot');
+	dot.templateSettings.strip = false;
 	var template = dot.template(this.templates["feed"], null, this.templates);
 
 	var FeedConfig = React.createClass({
 		render: function () {
-			return React.createElement("div", { className: "config" },
-				React.createElement("div", { className: "configItem" },
-					React.createElement("p", { className: "configDescription" },
-						"When displaying articles from this feed..."),
-					React.createElement("div", { className: "buttonList" },
-						React.createElement("div",
-							{ className: "button" + (this.props.feed.open_linked ? "" : " selected") },
-							"Show description from feed"),
-						React.createElement("div",
-							{ className: "button" + (this.props.feed.open_linked ? " selected" : "") },
-							"Embed linked article")
-					)
+			return React.createElement("div", { className: "configItem" },
+				React.createElement("p", { className: "configDescription" },
+					"When displaying articles from this feed..."),
+				React.createElement("div", { className: "buttonList" },
+					React.createElement("div",
+						{ className: "button" + (this.props.feed.open_linked ? "" : " selected") },
+						"Show description from feed"),
+					React.createElement("div",
+						{ className: "button" + (this.props.feed.open_linked ? " selected" : "") },
+						"Embed linked article")
 				)
 			);
 		}
