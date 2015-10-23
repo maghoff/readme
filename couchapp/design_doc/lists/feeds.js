1b7b58dedae8d7bc8216afb2f1c506f34522f64f
Magnus Hoff <maghoff@gmail.com>
2015-10-23 19:25:45 +0200

    Tweaks to feeds list function

--- couchapp/design_doc/lists/feeds.js
+++ couchapp/design_doc/lists/feeds.js
@@ -3,13 +3,17 @@ function (head, req) {
 
 	var FeedsList = React.createClass({
 		render: function () {
-			return React.createElement("ul", { className: "feeds" },
-				this.props.feeds.map(function (feed) {
-					return React.createElement("li", null,
-						React.createElement("a", { href: "feed/" + feed._id }, feed.title)
-					);
-				})
-			);
+			if (this.props.feeds.length) {
+				return React.createElement("ul", { className: "feeds" },
+					this.props.feeds.map(function (feed) {
+						return React.createElement("li", null,
+							React.createElement("a", { href: "feed/" + feed._id }, feed.title)
+						);
+					})
+				);
+			} else {
+				return React.createElement("p", null, "You are not subscribed to any feeds");
+			}
 		}
 	});
 
