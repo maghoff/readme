664bf5cc5648ba1b8822dca6f372a9ed5833812e
Magnus Hoff <maghoff@gmail.com>
2015-10-25 11:44:23 +0100

    Allow deletion of documents

--- couchapp/design_doc/validate_doc_update.js
+++ couchapp/design_doc/validate_doc_update.js
@@ -1,4 +1,6 @@
 function (newDoc, oldDoc, userCtx, secObj) {
+	if (newDoc._deleted) return;
+
 	if (newDoc.type === "feed") {
 		if (typeof newDoc.feed !== "string") throw({forbidden: "feed objects must have a feed URL"});
 		if (!newDoc.feed.match(/^https?:\/\//)) throw({forbidden: "feed scheme must be http or https"});
